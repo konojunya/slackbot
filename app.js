b2f2a258bccf61d65a3759b4a25a392431350947
konojunya <junya03dance@gmail.com>
2016-08-02 13:00:04 +0900

    15%しか返信してこないように

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var Botkit = require("botkit");
 var request = require("request");
+var Trello = require("node-trello");
+// var t = new Trello("031c348b6181692abd2bb24c6daa950a", "4420cb446b6cc5584b8746608fdae6679435734c7da019c5ee59bcc23748e119");
+// t.post("/1/boards",{due: null,name: "さとみん"},function(err,data){
+// 	console.log(data)
+// });
 var bot = Botkit.slackbot({
     debug: false
 });
@@ -10,8 +15,8 @@ bot.spawn({
 
 bot.hears('.*',['ambient'],function(bot,message) {
 	if(!(message.text.match(/^http/))){
-		var rand = Math.floor(Math.random()*10);
-		if(rand < 8 && rand%2 == 0){
+		var rand = Math.floor(Math.random()*100);
+		if(rand < 15){
 			var headers = {
 				'Content-Type':'application/json'
 			}
